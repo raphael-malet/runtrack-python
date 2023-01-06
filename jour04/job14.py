f5c8982846e65421b8f8d81c90174ed8f935a16d
raphael-malet <115145341+raphael-malet@users.noreply.github.com>
2023-01-06 01:24:00 +0100

    modification du job14
    modification du job14 permet d'afficher la phrase apres supression des mot ciblé

--- jour04/job14.py
+++ jour04/job14.py
@@ -4,16 +4,10 @@ my_long_word = ("La peur est le chemin vers le côté obscur la peur mène à la
 L = my_long_word.split()
 n = int(input('Supprimer les mots plus petit que ?: '))
 
-
 def suppression_mot(liste,n):
     for i in liste[::]:
         if i[::] <= i[0:n]:
             liste.remove(i)
     print(liste)
-    for j in liste[::]:
-        string = j
-
-            
-    print (string)
-    
-suppression_mot(L, n)
+    return" ".join(liste)    
+print(suppression_mot(L, n))
